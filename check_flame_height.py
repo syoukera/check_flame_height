ce2cff959ec410797a8c08a66bcaa011e929e5bf
syoukera <s.akira2986@gmail.com>
2020-02-27 08:14:44 +0900

    Update check_flame_height.py: add arg

--- check_flame_height.py
+++ check_flame_height.py
@@ -2,10 +2,16 @@ import numpy as np
 import cv2
 import os
 from tqdm import tqdm
+import sys
 
-base_path = input('Please enter the path of image directory (or use . to use default setting)')
-if base_path == '.':
-    base_path = '../C001H001S0003_8bit'
+args = sys.argv
+
+if len(args) > 1:
+    base_path = args[1]
+else:
+    base_path = input('Please enter the path of image directory (or use . to use default setting)')
+    if base_path == '.':
+        base_path = '../../d20mm_Air20SLM_f1.0_C001H001S0001'
 
 files = os.listdir(base_path)
 files = files[1:]
@@ -20,12 +26,14 @@ print('get iy value at ix = ' + str(ix_center))
 for i ,file in enumerate(files):
     
     # averate 10 images in each 10 image sequence
-    if i/10 > 10:
-        break
-    if i%10 != 0: 
-        continue
+    # if i/10 > 10:
+    #     break
+    # if i%10 != 0: 
+    #     continue
 
     path = os.path.join(base_path, file)
+    assert os.path.exists(path), path + " is not exists"
+    
     img = cv2.imread(path, cv2.IMREAD_COLOR)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img_blue = img[:, :, 2]
